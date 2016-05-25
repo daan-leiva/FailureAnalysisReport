d7b06098412e3f4b4831d2d90ba01e28e17ba333
unknown <dleiva@ATI-PC063.ati.local>
2016-05-25 03:41:33 -0700

    Initial commit

---
+++ FailureAnalysisReport/FailureAnalysisReport.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace FailureAnalysisReport
+{
+    partial class FailureAnalysisReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.startDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.endDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ncDataButton = new System.Windows.Forms.Button();
+            this.cpaDataButton = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.rowsLabel = new System.Windows.Forms.Label();
+            this.ViewHoursButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.estimatedHoursTextBox = new System.Windows.Forms.TextBox();
+            this.combinedTimeTrackingButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Failure Analysis Report";
+            // 
+            // startDateTimePicker
+            // 
+            this.startDateTimePicker.Location = new System.Drawing.Point(49, 103);
+            this.startDateTimePicker.Name = "startDateTimePicker";
+            this.startDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.startDateTimePicker.TabIndex = 1;
+            this.startDateTimePicker.ValueChanged += new System.EventHandler(this.startDateTimePicker_ValueChanged);
+            // 
+            // endDateTimePicker
+            // 
+            this.endDateTimePicker.Location = new System.Drawing.Point(49, 168);
+            this.endDateTimePicker.Name = "endDateTimePicker";
+            this.endDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.endDateTimePicker.TabIndex = 2;
+            this.endDateTimePicker.ValueChanged += new System.EventHandler(this.endDateTimePicker_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(46, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Start Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(49, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "End Date";
+            // 
+            // ncDataButton
+            // 
+            this.ncDataButton.Location = new System.Drawing.Point(282, 111);
+            this.ncDataButton.Name = "ncDataButton";
+            this.ncDataButton.Size = new System.Drawing.Size(75, 67);
+            this.ncDataButton.TabIndex = 5;
+            this.ncDataButton.Text = "View NC Data";
+            this.ncDataButton.UseVisualStyleBackColor = true;
+            this.ncDataButton.Click += new System.EventHandler(this.ncDataButton_Click);
+            // 
+            // cpaDataButton
+            // 
+            this.cpaDataButton.Location = new System.Drawing.Point(363, 111);
+            this.cpaDataButton.Name = "cpaDataButton";
+            this.cpaDataButton.Size = new System.Drawing.Size(75, 67);
+            this.cpaDataButton.TabIndex = 6;
+            this.cpaDataButton.Text = "View CPA Data";
+            this.cpaDataButton.UseVisualStyleBackColor = true;
+            this.cpaDataButton.Click += new System.EventHandler(this.cpaDataButton_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(49, 242);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(1153, 503);
+            this.dataGridView.TabIndex = 7;
+            // 
+            // rowsLabel
+            // 
+            this.rowsLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.rowsLabel.AutoSize = true;
+            this.rowsLabel.Location = new System.Drawing.Point(1134, 752);
+            this.rowsLabel.Name = "rowsLabel";
+            this.rowsLabel.Size = new System.Drawing.Size(37, 13);
+            this.rowsLabel.TabIndex = 8;
+            this.rowsLabel.Text = "Rows:";
+            // 
+            // ViewHoursButton
+            // 
+            this.ViewHoursButton.Location = new System.Drawing.Point(444, 111);
+            this.ViewHoursButton.Name = "ViewHoursButton";
+            this.ViewHoursButton.Size = new System.Drawing.Size(75, 67);
+            this.ViewHoursButton.TabIndex = 9;
+            this.ViewHoursButton.Text = "View Failure Analysis";
+            this.ViewHoursButton.UseVisualStyleBackColor = true;
+            this.ViewHoursButton.Click += new System.EventHandler(this.ViewHoursButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(540, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Estimated Total Hours:";
+            // 
+            // estimatedHoursTextBox
+            // 
+            this.estimatedHoursTextBox.Location = new System.Drawing.Point(660, 207);
+            this.estimatedHoursTextBox.Name = "estimatedHoursTextBox";
+            this.estimatedHoursTextBox.Size = new System.Drawing.Size(120, 20);
+            this.estimatedHoursTextBox.TabIndex = 11;
+            this.estimatedHoursTextBox.TextChanged += new System.EventHandler(this.estimatedHoursTextBox_TextChanged);
+            // 
+            // combinedTimeTrackingButton
+            // 
+            this.combinedTimeTrackingButton.Location = new System.Drawing.Point(525, 111);
+            this.combinedTimeTrackingButton.Name = "combinedTimeTrackingButton";
+            this.combinedTimeTrackingButton.Size = new System.Drawing.Size(75, 67);
+            this.combinedTimeTrackingButton.TabIndex = 12;
+            this.combinedTimeTrackingButton.Text = "Time Tracking Combined PBMA";
+            this.combinedTimeTrackingButton.UseVisualStyleBackColor = true;
+            this.combinedTimeTrackingButton.Click += new System.EventHandler(this.combinedTimeTrackingButton_Click);
+            // 
+            // FailureAnalysisReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 778);
+            this.Controls.Add(this.combinedTimeTrackingButton);
+            this.Controls.Add(this.estimatedHoursTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ViewHoursButton);
+            this.Controls.Add(this.rowsLabel);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.cpaDataButton);
+            this.Controls.Add(this.ncDataButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.endDateTimePicker);
+            this.Controls.Add(this.startDateTimePicker);
+            this.Controls.Add(this.label1);
+            this.Name = "FailureAnalysisReport";
+            this.Text = "Failure Analysis Report";
+            this.Load += new System.EventHandler(this.FailureAnalysisReport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker startDateTimePicker;
+        private System.Windows.Forms.DateTimePicker endDateTimePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ncDataButton;
+        private System.Windows.Forms.Button cpaDataButton;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label rowsLabel;
+        private System.Windows.Forms.Button ViewHoursButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox estimatedHoursTextBox;
+        private System.Windows.Forms.Button combinedTimeTrackingButton;
+    }
+}
+
---
+++ FailureAnalysisReport/FailureAnalysisReport.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace FailureAnalysisReport
+{
+    public partial class FailureAnalysisReport : Form
+    {
+        string connection_string = "Server=ATI-SQL;Database=uniPoint_Live;UID=jbread;PWD=Cloudy2Day";
+        private BindingSource bindingSource = new BindingSource();
+        enum ButtonType { NCData, CPAData, ViewHours,  Combined};
+        private ButtonType lastButtonClicked = ButtonType.NCData;
+
+
+        public FailureAnalysisReport()
+        {
+            InitializeComponent();
+        }
+
+        private void FailureAnalysisReport_Load(object sender, EventArgs e)
+        {
+            dataGridView.DataSource = bindingSource;
+            dataGridView.ReadOnly = true;
+
+            // initialize dateTime object to a year before today
+            startDateTimePicker.Value = DateTime.Now.AddYears(-1);
+
+            // initialize estimated hours to 2000 and disable
+            estimatedHoursTextBox.Text = "2000";
+            estimatedHoursTextBox.Enabled = false;
+        }
+
+        private void ncDataButton_Click(object sender, EventArgs e)
+        {
+            string query =
+                "SELECT NCR, Investigated_by AS 'Investigated By', Investigation_hrs AS 'Investigation Hours', Material AS 'Part No', ncT.Job AS 'Job', Investigation_date AS 'Investigation Date' , Investigation_complete AS 'Investigation Complete', Origin_ref AS 'Origin Reference', Origin_cause AS 'Origin Cause', Origin_category AS 'Origin Category', tT.Notes AS 'Investigation Notes'\n" +
+                "FROM uniPoint_Live.dbo.PT_NC AS ncT\n" +
+                "LEFT JOIN uniPoint_Live.dbo.PT_Task AS tT\n" +
+                "ON tT.TaskType = 'NC' AND tT.TaskTypeID = NCR AND tT.TaskSubType = 'Investigation'\n" +
+                "WHERE CONVERT(DATE, NCR_Date) > CONVERT(Date, '" + startDateTimePicker.Value.ToShortDateString() + "') AND  CONVERT(DATE, NCR_Date) <= CONVERT(Date, '" + endDateTimePicker.Value.ToShortDateString() + "')\n" +
+                "ORDER BY NCR;";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection_string);
+
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+            bindingSource.DataSource = dt;
+
+            // update Rows label
+            rowsLabel.Text = "Rows: " + dt.Rows.Count;
+
+            // update last button clicked
+            lastButtonClicked = ButtonType.NCData;
+
+            // estimated hours disable
+            estimatedHoursTextBox.Enabled = false;
+        }
+
+        private void cpaDataButton_Click(object sender, EventArgs e)
+        {
+            string query =
+                "SELECT CPA_no AS 'CPA', Investigation_by AS 'Investigated By', Investigation_hrs AS 'Investigation Hours', cpT.Description AS 'Part No/ Description', Investigation_date AS 'Investigation Date', Investigation_complete AS 'Investigation Complete', Origin_ref AS 'Origin Reference', Origin_cause AS 'Origin Cause', Origin_category AS 'Origin Category', tT.Notes AS 'Investigation Notes'\n" +
+                "FROM uniPoint_Live.dbo.PT_CPA AS cpT\n" +
+                "LEFT JOIN uniPoint_Live.dbo.PT_Task AS tT\n" +
+                "ON tT.TaskType = 'CPA' AND tT.TaskTypeID = CPA_no AND tT.TaskSubType = 'Investigation'\n" +
+                "WHERE CONVERT(DATE, CPA_date) > CONVERT(Date, '" + startDateTimePicker.Value.ToShortDateString() + "') AND  CONVERT(DATE, CPA_date) <= CONVERT(Date, '" + endDateTimePicker.Value.ToShortDateString() + "')\n" +
+                "ORDER BY CPA_no;";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection_string);
+
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+            bindingSource.DataSource = dt;
+
+            // update Rows label
+            rowsLabel.Text = "Rows: " + dt.Rows.Count;
+
+            // update last button clicked
+            lastButtonClicked = ButtonType.CPAData;
+
+            // estimated hours disable
+            estimatedHoursTextBox.Enabled = false;
+        }
+
+
+        private void ViewHoursButton_Click(object sender, EventArgs e)
+        {
+            // check if empty textbox
+            if (estimatedHoursTextBox.Text.Length == 0)
+                return;
+
+            // check that estimated total hours contains an integer
+            int estimatedHours = -100;
+            if (!int.TryParse(estimatedHoursTextBox.Text, out estimatedHours) || estimatedHours < 0)
+            {
+                MessageBox.Show("Invalid number of estimated hours");
+                return;
+            }
+
+            string query =
+                "SELECT Investigator AS 'Investigated By', CONVERT(DECIMAL(18,2), SUM(hours)) AS 'Total Hours', " + estimatedHours + " AS 'Estimated Total Hours', CONVERT(varchar, CONVERT(DECIMAL(18,2), (CONVERT(DECIMAL, SUM(hours))/" + estimatedHours + ")*100)) + '%' AS Percentage, (CASE WHEN (CONVERT(DECIMAL, SUM(hours))/" + estimatedHours + ") > 0.01 THEN CONVERT(varchar, CONVERT(DECIMAL(18), ROUND((CONVERT(DECIMAL, SUM(hours))/" + estimatedHours + ")*100, 0))) + '%' ELSE '0%' END) AS 'Post-Threshold Percentage for Failure Analysis Lab'\n" +
+                "FROM\n" +
+                "(SELECT Investigated_by AS Investigator, Investigation_hrs AS hours FROM uniPoint_Live.dbo.PT_NC\n" +
+                "WHERE CONVERT(DATE, NCR_Date) > CONVERT(Date, '" + startDateTimePicker.Value.ToShortDateString() + "') AND  CONVERT(DATE, NCR_Date) <= CONVERT(Date, '" + endDateTimePicker.Value.ToShortDateString() + "')\n" +
+                "UNION ALL\n" +
+                "SELECT Investigation_by AS Investigator, Investigation_hrs AS hours FROM uniPoint_Live.dbo.PT_CPA\n" +
+                "WHERE CONVERT(DATE, CPA_date) > CONVERT(Date, '" + startDateTimePicker.Value.ToShortDateString() + "') AND  CONVERT(DATE, CPA_date) <= CONVERT(Date, '" + endDateTimePicker.Value.ToShortDateString() + "')\n" +
+                ") AS combinedT\n" +
+                "WHERE Investigator IS NOT NULL\n" +
+                "GROUP BY Investigator\n" +
+                "ORDER BY Investigator;";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection_string);
+
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+            dt.Rows.Add();
+            dt.Rows[dt.Rows.Count - 1].SetField(0, "Total");
+
+            double total = 0;
+            for(int i = 0; i < dt.Rows.Count - 1; i++)
+            {
+                total += double.Parse(dt.Rows[i][1].ToString());
+            }
+            dt.Rows[dt.Rows.Count - 1].SetField(1, total);
+            bindingSource.DataSource = dt;
+
+            // update Rows label
+            rowsLabel.Text = "Rows: " + dt.Rows.Count;
+
+            // update last button clicked
+            lastButtonClicked = ButtonType.ViewHours;
+
+            // enable estimated hours
+            estimatedHoursTextBox.Enabled = true;
+        }
+
+
+
+        private void combinedTimeTrackingButton_Click(object sender, EventArgs e)
+        {
+            string query =
+                "SELECT TrackingProgram AS 'Tracking Program', nccpaNum AS 'NC# / CPA#', Investigator AS 'Investigated By', firstLetterOfFirstName AS 'First Letter of First Name', lastName AS 'Last Name', hours AS 'Investigation Hours', partNum AS 'Part #', Job AS Job, investigationDate AS 'Investigation Date', investigationComplete AS 'Investigation Complete', originRef AS 'Origin Reference', originCause AS 'Origin Cause', originCategory AS 'Origin Category', Notes\n" +
+                "FROM\n" +
+                "(SELECT 'NC' AS TrackingProgram, NCR AS nccpaNum, Investigated_by AS Investigator, LEFT(Investigated_by, 1) AS firstLetterOfFirstName, SUBSTRING(Investigated_by, 2, LEN(Investigated_by) - 1) AS lastName,Investigation_hrs AS hours, Material AS partNum, Job AS Job, Investigation_date AS investigationDate, Investigation_complete AS investigationComplete, Origin_ref AS originRef, Origin_cause AS originCause, Origin_category AS originCategory, tT.Notes AS notes FROM uniPoint_Live.dbo.PT_NC\n" +
+                "LEFT JOIN uniPoint_Live.dbo.PT_Task AS tT\n" +
+                "ON tT.TaskType = 'NC' AND tT.TaskTypeID = NCR AND tT.TaskSubType = 'Investigation'\n" +
+                "WHERE CONVERT(DATE, NCR_Date) > CONVERT(Date, '" + startDateTimePicker.Value.ToShortDateString() + "') AND  CONVERT(DATE, NCR_Date) <= CONVERT(Date, '" + endDateTimePicker.Value.ToShortDateString() + "')\n" +
+                "UNION ALL\n" +
+                "SELECT 'CPA' AS TrackingProgram, CPA_no AS nccpaNum, Investigation_by AS Investigator, LEFT(Investigation_by, 1) AS firstLetterOfFirstName, SUBSTRING(Investigation_by, 2, LEN(Investigation_by) - 1) AS lastName,Investigation_hrs AS hours, cpT.Description AS partNum, 'Not Part of CPA Data' AS Job, Investigation_date AS investigationDate, Investigation_complete AS investigationComplete, Origin_ref AS originRef, Origin_cause AS originCause, Origin_category AS originCategory, tT.Notes AS notes FROM uniPoint_Live.dbo.PT_CPA AS cpT\n" +
+                "LEFT JOIN uniPoint_Live.dbo.PT_Task AS tT\n" +
+                "ON tT.TaskType = 'CPA' AND tT.TaskTypeID = CPA_no AND tT.TaskSubType = 'Investigation'\n" +
+                "WHERE CONVERT(DATE, CPA_date) > CONVERT(Date, '" + startDateTimePicker.Value.ToShortDateString() + "') AND  CONVERT(DATE, CPA_date) <= CONVERT(Date, '" + endDateTimePicker.Value.ToShortDateString() + "')\n" +
+                ") AS combinedT\n" +
+                "ORDER BY [Tracking Program], nccpaNum;";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection_string);
+
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+            bindingSource.DataSource = dt;
+
+            // update Rows label
+            rowsLabel.Text = "Rows: " + dt.Rows.Count;
+
+            // update last button clicked
+            lastButtonClicked = ButtonType.Combined;
+
+            // disable
+            estimatedHoursTextBox.Enabled = false;
+        }
+
+        private void startDateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            switch (lastButtonClicked)
+            {
+                case ButtonType.NCData:
+                    ncDataButton_Click(new object(), new EventArgs());
+                    break;
+                case ButtonType.CPAData:
+                    cpaDataButton_Click(new object(), new EventArgs());
+                    break;
+                case ButtonType.ViewHours:
+                    ViewHoursButton_Click(new object(), new EventArgs());
+                    break;
+                case ButtonType.Combined:
+                    combinedTimeTrackingButton_Click(new object(), new EventArgs());
+                    break;
+            }
+        }
+
+        private void endDateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            switch (lastButtonClicked)
+            {
+                case ButtonType.NCData:
+                    ncDataButton_Click(new object(), new EventArgs());
+                    break;
+                case ButtonType.CPAData:
+                    cpaDataButton_Click(new object(), new EventArgs());
+                    break;
+                case ButtonType.ViewHours:
+                    ViewHoursButton_Click(new object(), new EventArgs());
+                    break;
+                case ButtonType.Combined:
+                    combinedTimeTrackingButton_Click(new object(), new EventArgs());
+                    break;
+            }
+        }
+
+        private void estimatedHoursTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (lastButtonClicked == ButtonType.ViewHours)
+                ViewHoursButton_Click(new object(), new EventArgs());
+        }
+    }
+}
